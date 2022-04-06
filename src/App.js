c0b6ac6a9ec5d70b4f1b999e2680ef460cbd11ac
GiovaneForlenza <giovane.franciscoforl@gmail.com>
2022-04-06 18:05:22 -0400

    changed stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react'
+import { FaAngleDoubleRight, FaGithub } from 'react-icons/fa'
+import data from './data'
+import { AiFillGithub } from 'react-icons/ai'
+import { MdOpenInNew } from 'react-icons/md'
+const url = 'https://course-api.com/react-tabs-project'
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const newJobs = data;
+    // const response = await fetch(url);
+    // const newJobs = await response.json();
+    setJobs(newJobs);
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchJobs();
+  }, [])
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    )
+  }
+
+  const { company, dates, duties, title, demoLink, githubLink, img } = jobs[value];
+
+  console.log({ img });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="jobs-center">
+        <div className='btn-container'>
+          {jobs.map((item, index) => {
+            return <button key={item.id} onClick={() => setValue(index)} className={`job-btn ${index === value && 'active-btn'}`}>{item.title} </button>
+          })}
+        </div>
+        <article className="job-info">
+          <h3>{title}</h3>
+          <h4>{company}</h4>
+          {/* <p className="job-date">{dates}</p> */}
+          {duties.map((duty, index) => {
+            return <div className="job-desc" key={index}>
+              <FaAngleDoubleRight className='job-icon' />
+              <p>{duty}</p>
+            </div>
+          })}
+          <div className="img">
+            <img src={img} alt={title} />
+          </div>
+          <a href={demoLink} target="_blank"><MdOpenInNew /> View Demo</a>
+          <a href={githubLink} target="_blank"><MdOpenInNew /> View on GitHub</a>
+        </article>
+      </div>
+    </section>
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,95 @@
+const data = [
+  // Restaurant Menu
+  {
+    id: "I9UYiWftXx",
+    order: 1,
+    title: "Restaurant Menu",
+    dates: "May 2014 - September 2015",
+    duties: [
+      "Display a list of pre-selected recipes, with basic information in the home page.",
+      "User can toggle which category they want to see by pressing the buttons in the top, making the list sort the items in that category.",
+      "Each recipe has a button that takes the user to a specific page, containing more information about the recipe, ingredients and step-by-step instructions.",
+    ],
+    company: "React",
+    demoLink: "https://recepie-book.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/recipe-book",
+    img: "./images/restaurant-menu.png",
+  },
+  // Cocktail Selection
+  {
+    id: "YRO1t6KIgZ",
+    order: 1,
+    title: "Cocktail Selection",
+    dates: "May 2014 - September 2015",
+    duties: [
+      "Project uses React components alongside with an external API to fetch information about cocktails.",
+      "User can search for a specific cocktail and clicking the ‘details’ button will redirect the user to a page with more information about the cocktail.",
+    ],
+    company: "React",
+    demoLink: "https://react-project-cocktaildb.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/cocktail-db",
+    img: "./images/cocktail.png",
+  },
+  // Product Review
+  {
+    id: "4jgSfl41Va",
+    order: 3,
+    title: "Product Review",
+    dates: "December 2015 - Present",
+    duties: [
+      "Project uses React elements to display a list of reviews from a product or service, displaying the image, name, job and quote left in the review.",
+      "User can navigate trough the reviews pressing the buttons or select a random review clicking in the 'Surprise Me' button.",
+    ],
+    company: "React",
+    demoLink: "https://react-project-reviews-app.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/product-review",
+    img: "./images/review-app.png",
+  },
+  // Color HUE Generator
+  {
+    id: "4tjNWlYmLM",
+    order: 2,
+    title: "Color HUE Generator",
+    dates: "May 2015 - December 2015",
+    duties: [
+      "This projects uses an external library to generate variations of a HEX color, and places it into Reject elements.",
+      "Generates 20 variations of a single color, with a 10% variation on the HUE of the color.",
+    ],
+    company: "React",
+    demoLink: "https://react-project-colorgenerator.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/color-hue-generator",
+    img: "./images/color-hue.png",
+  },
+  // Accordions
+  {
+    id: "9JMkYMPw6I",
+    order: 1,
+    title: "Accordions",
+    dates: "May 2014 - September 2015",
+    duties: [
+      "Presents how a list of accordions would work, with the ability to show/hide information on click.",
+      "Titles and descriptions on each accordion is easily editable with a Reject element.",
+    ],
+    company: "React",
+    demoLink: "https://react-projects-accordions.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/accordions",
+    img: "./images/accordions.png",
+  },
+  // Grocery List
+  {
+    id: "QlN3unOX5J",
+    order: 1,
+    title: "Grocery List",
+    dates: "May 2014 - September 2015",
+    duties: [
+      "Project created using React allows user to Create, Edit and Delete items from a list.",
+      "Designed to save the list into the browser’s local storage.",
+    ],
+    company: "React",
+    demoLink: "https://react-projects-grocery-list.netlify.app/",
+    githubLink: "https://github.com/GiovaneForlenza/grocery-list",
+    img: "./images/grocery-list.png",
+  },
+];
+
+export default data;
